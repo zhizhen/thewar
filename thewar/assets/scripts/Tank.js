edf251d7782c2d109b6a97988479625f5a72cddd
zhizhen <zhzhzhen@gmail.com>
2016-04-05 18:28:50 +0800

    fix

--- thewar/assets/scripts/Tank.js
+++ thewar/assets/scripts/Tank.js
@@ -3,12 +3,14 @@ cc.Class({
 
     properties: {
         tankPrefeb: cc.Prefab,
+        bullet: cc.Prefab
     },
 
     // use this for initialization
     onLoad: function () {
         console.log("tank view onload!");
         this.create();
+        this.faceAndfire(400, 600)
     },
 
     // called every frame, uncomment this function to activate update callback
@@ -17,10 +19,24 @@ cc.Class({
     // },
     
     create: function () {
-        var tankNode = cc.instantiate(this.tankPrefeb);
-        tankNode.position = cc.p(-150, -400);
+        this.tankNode = cc.instantiate(this.tankPrefeb);
+        this.tankNode.position = cc.p(-150, -400);
         console.log("tank view create!");
-        this.node.addChild(tankNode);
+        this.node.addChild(this.tankNode);
         console.log("tank view create!");
+    },
+    
+    faceAndfire: function(x, y) {
+        var gun = cc.find("tankInfo/vehicle/gun", this.tankNode);
+        var rotate = cc.rotateBy(1, 90, 0);
+
+        var tmp = this
+        gun.runAction(cc.sequence(rotate, cc.callFunc(function() {
+            console.log("on create bullet!");
+            var bullet = cc.instantiate(tmp.bullet);
+            bullet.position = cc.p(-150, -400);
+            tmp.node.addChild(bullet);
+        })));
+        
     }
 });
