cfb381e3f9d315fecdc591283711d83d305e1484
张植臻 <zhizhen@MacAir.lan>
2016-04-05 22:45:44 +0800

    坦克炮塔旋转

--- thewar/assets/scripts/Tank.js
+++ thewar/assets/scripts/Tank.js
@@ -1,8 +1,13 @@
+var Utils = require("Utils");
+
 cc.Class({
     extends: cc.Component,
 
     properties: {
-        speed: 20,
+        move_speed: 20,
+        gun_speed: 90, // 炮管每秒旋转角度
+        vehicle_angle: 0, // 车身角度
+        gun_angle: 0, // 炮管相对车身朝向角度
         tankPrefeb: cc.Prefab,
         bullet: cc.Prefab
     },
@@ -10,7 +15,7 @@ cc.Class({
     // use this for initialization
     onLoad: function () {
         console.log("tank view onload!");
-        this.create(200, 300);
+        this.create(200, 300, 0);
         this.faceAndfire(400, 600)
     },
 
@@ -19,7 +24,8 @@ cc.Class({
 
     // },
     
-    create: function (x, y) {
+    create: function (x, y, face) {
+        this.vehicle_angle = 180 * face / Math.PI;
         this.tankNode = cc.instantiate(this.tankPrefeb);
         this.tankNode.position = cc.p(x, y);
         console.log("tank view create!");
@@ -28,6 +34,7 @@ cc.Class({
     },
     
     move: function (x, y) {
+        
         var old_pos = this.tankNode.position;
         var angle = 180 * Math.atan((y - old_pos.y)/(x - old_pos.x)) / Math.PI;
         var turn = cc.rotateBy(0.4, angle);
@@ -39,16 +46,33 @@ cc.Class({
 
     faceAndfire: function(x, y) {
         var gun = cc.find("tankInfo/vehicle/gun", this.tankNode);
-        var old_pos = this.tankNode.position;
-        var angle = 180 * Math.atan((y - old_pos.y)/(x - old_pos.x)) / Math.PI;
-        var rotate = cc.rotateBy(1, angle, 0);
+        
+        var old_pos = cc.p(this.tankNode.position.x + gun.position.x, 
+            this.tankNode.position.y + gun.position.y);
+        var now_angle = this.vehicle_angle + this.gun_angle;
+        var degree = Math.atan2(x - old_pos.x, y - old_pos.y);
+        var turn_angle = Utils.fmod(180 * (degree - now_angle) / Math.PI, 360);
+        var rotate = cc.rotateBy(Math.abs(turn_angle) / this.gun_speed, turn_angle, 0);
+        console.log("now_angle:" + now_angle *180 / Math.PI
+        + ";degree:" + degree * 180 / Math.PI
+        + ";turn_angle:" + turn_angle);
 
-        var tmp = this
-        gun.runAction(cc.sequence(rotate, cc.callFunc(function() {
-            console.log("on create bullet!");
-            var bullet = cc.instantiate(tmp.bullet);
-            bullet.position = cc.p(-150, -400);
-            tmp.node.addChild(bullet);
-        })));
+        if (degree - now_angle !== 0) {
+            this.gun_angle = this.gun_angle + turn_angle * Math.PI / 180;
+            var tmp = this
+            gun.runAction(cc.sequence(rotate, cc.callFunc(function() {
+                tmp.fire();
+            })));
+        }
+        else{
+            this.fire();
+        }
+    },
+    
+    fire: function() {
+        console.log("fire the hole!");
+        var bullet = cc.instantiate(this.bullet);
+        bullet.position = cc.p(-150, -400);
+        this.node.addChild(bullet);
     }
 });
--- thewar/assets/scripts/Touch.js
+++ thewar/assets/scripts/Touch.js
@@ -10,12 +10,15 @@ cc.Class({
         // this.streak = cc.MotionStreak.create(0.3, 3, 30, cc.c3b(50, 220, 255), "blade.png");
         // this.node.addChild(this.streak);
         var begin = function (event){
-            console.log("touchend begin:"+event.touch.getLocation().x +"|"+event.touch.getLocation().y)
+            console.log("touchend begin:"+event.touch.getLocation().x +"|"+event.touch.getLocation().y);
+            var location = event.touch.getLocation();
+            this.refToTank.faceAndfire(location.x, location.y);
         }
         var move = function (event){
-            console.log("touchend move:"+event.touch.getLocation().x +"|"+event.touch.getLocation().y)
+            console.log("touchend move:"+event.touch.getLocation().x +"|"+event.touch.getLocation().y);
+            var location = event.touch.getLocation();
             // this.streak.setPosition(event.touch.getLocation().x, event.touch.getLocation().y)
-            
+            this.refToTank.faceAndfire(location.x, location.y);
         }
         var end = function (event){
             var location = event.touch.getLocation();
