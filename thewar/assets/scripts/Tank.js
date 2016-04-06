95299567afc37cdb27c656afb5e4d278cc7f2e37
zhizhen <zhzhzhen@gmail.com>
2016-04-06 17:32:43 +0800

    gun face target rotate

--- thewar/assets/scripts/Tank.js
+++ thewar/assets/scripts/Tank.js
@@ -5,9 +5,7 @@ cc.Class({
 
     properties: {
         move_speed: 20,
-        gun_speed: 90, // 炮管每秒旋转角度
-        vehicle_angle: 0, // 车身角度
-        gun_angle: 0, // 炮管相对车身朝向角度
+        gun_speed: 0, // 炮管每秒旋转角度
         tankPrefeb: cc.Prefab,
         bullet: cc.Prefab
     },
@@ -16,7 +14,7 @@ cc.Class({
     onLoad: function () {
         console.log("tank view onload!");
         this.create(200, 300, 0);
-        this.faceAndfire(400, 600)
+        this.face(400, 600)
     },
 
     // called every frame, uncomment this function to activate update callback
@@ -25,12 +23,12 @@ cc.Class({
     // },
     
     create: function (x, y, face) {
-        this.vehicle_angle = 180 * face / Math.PI;
         this.tankNode = cc.instantiate(this.tankPrefeb);
         this.tankNode.position = cc.p(x, y);
-        console.log("tank view create!");
+        // console.log("tank view create!");
         this.node.addChild(this.tankNode);
-        console.log("tank view create!");
+        var vehicle = cc.find("tankInfo/vehicle", this.tankNode);
+        vehicle.rotationX = face;
     },
     
     move: function (x, y) {
@@ -44,35 +42,66 @@ cc.Class({
         this.tankNode.runAction(move);
     },
 
-    faceAndfire: function(x, y) {
+    face: function(x, y) {
         var gun = cc.find("tankInfo/vehicle/gun", this.tankNode);
+        var vehicle = cc.find("tankInfo/vehicle", this.tankNode);
+        var ACTION_TAG = 1;
+        // 通过 tag 停止一个动作
+        gun.stopActionByTag(ACTION_TAG);
         
         var old_pos = cc.p(this.tankNode.position.x + gun.position.x, 
             this.tankNode.position.y + gun.position.y);
-        var now_angle = this.vehicle_angle + this.gun_angle;
+        var gun_angle = gun.getRotation();
+        var vehicle_angle = vehicle.getRotation();
+        var now_angle = vehicle_angle + Math.PI * gun_angle / 180;
         var degree = Math.atan2(x - old_pos.x, y - old_pos.y);
         var turn_angle = Utils.fmod(180 * (degree - now_angle) / Math.PI, 360);
         var rotate = cc.rotateBy(Math.abs(turn_angle) / this.gun_speed, turn_angle, 0);
-        console.log("now_angle:" + now_angle *180 / Math.PI
-        + ";degree:" + degree * 180 / Math.PI
-        + ";turn_angle:" + turn_angle);
+        // 给 action 设置 tag
+        rotate.setTag(ACTION_TAG);
+        
+        // console.log("now_angle:" + now_angle *180 / Math.PI
+        // + ";degree:" + degree * 180 / Math.PI
+        // + ";turn_angle:" + turn_angle);
 
         if (degree - now_angle !== 0) {
-            this.gun_angle = this.gun_angle + turn_angle * Math.PI / 180;
-            var tmp = this
             gun.runAction(cc.sequence(rotate, cc.callFunc(function() {
-                tmp.fire();
+                console.log("face callback");
             })));
-        }
-        else{
-            this.fire();
         }
     },
     
     fire: function() {
         console.log("fire the hole!");
         var bullet = cc.instantiate(this.bullet);
-        bullet.position = cc.p(-150, -400);
+        bullet.position = cc.p(320, 480);
         this.node.addChild(bullet);
+    },
+    
+    isHit: function(p) {
+        var vehicle = cc.find("tankInfo/vehicle", this.tankNode);
+        var location = this.tankNode.position;
+        var vehicle_angle = vehicle.getRotation();
+        var face = 180 * vehicle_angle / Math.PI;
+        var unit_vect_h = cc.p(Math.sin(face), Math.cos(face));
+        var unit_vect_w = cc.p(Math.cos(face), - Math.sin(face));
+        
+        var width = vehicle.getContentSize().width;
+        var height = vehicle.getContentSize().height;
+        
+        // console.log("location,x:" + location.x + ";y:" + location.y);
+        
+        var p1 = cc.pAdd(cc.pAdd(location, cc.pMult(unit_vect_h, height / 2)), cc.pMult(cc.pNeg(unit_vect_w), width / 2));
+        var p2 = cc.pAdd(cc.pAdd(location, cc.pMult(cc.pNeg(unit_vect_h), height / 2)), cc.pMult(cc.pNeg(unit_vect_w), width / 2));
+        var p3 = cc.pAdd(cc.pAdd(location, cc.pMult(cc.pNeg(unit_vect_h), height / 2)), cc.pMult(unit_vect_w, width / 2));
+        var p4 = cc.pAdd(cc.pAdd(location, cc.pMult(unit_vect_h, height / 2)), cc.pMult(unit_vect_w, width / 2));
+        
+        // console.log("p1,x:" + p1.x + ";y:" + p1.y);
+        // console.log("p2,x:" + p2.x + ";y:" + p2.y);
+        // console.log("p3,x:" + p3.x + ";y:" + p3.y);
+        // console.log("p4,x:" + p4.x + ";y:" + p4.y);
+        
+        var getCross = function(p1, p2, p) {return (p2.x - p1.x) * (p.y - p1.y) - (p.x - p1.x) * (p2.y - p1.y);}
+        return getCross(p1, p2, p) * getCross(p3, p4, p) >= 0 && getCross(p2, p3, p) * getCross(p4, p1, p) >= 0;
     }
 });
--- thewar/assets/scripts/Touch.js
+++ thewar/assets/scripts/Touch.js
@@ -7,25 +7,39 @@ cc.Class({
 
     // use this for initialization
     onLoad: function () {
-        // this.streak = cc.MotionStreak.create(0.3, 3, 30, cc.c3b(50, 220, 255), "blade.png");
-        // this.node.addChild(this.streak);
+        
         var begin = function (event){
-            console.log("touchend begin:"+event.touch.getLocation().x +"|"+event.touch.getLocation().y);
+            // console.log("touchend begin:"+event.touch.getLocation().x +"|"+event.touch.getLocation().y);
             var location = event.touch.getLocation();
-            this.refToTank.faceAndfire(location.x, location.y);
+            // todo 判断，如果点击区域是车身，则接下来move是移动
+            // 否则，则是炮塔转动到瞄准该位置
+            var isHit = this.refToTank.isHit(location);
+            if (isHit){
+                // console.log("start move!");
+                this.pre_move = [];
+            }
+            else{
+                // console.log("face target!");
+                this.refToTank.face(location.x, location.y);
+            }
         }
         var move = function (event){
-            console.log("touchend move:"+event.touch.getLocation().x +"|"+event.touch.getLocation().y);
+            // console.log("touchend move:"+event.touch.getLocation().x +"|"+event.touch.getLocation().y);
             var location = event.touch.getLocation();
-            // this.streak.setPosition(event.touch.getLocation().x, event.touch.getLocation().y)
-            this.refToTank.faceAndfire(location.x, location.y);
+            // todo 移动车身，或者旋转炮塔
+            if (this.pre_move !== undefined){
+                this.pre_move.push(location);
+            }
+            else{
+                this.refToTank.face(location.x, location.y);
+            }
         }
         var end = function (event){
             var location = event.touch.getLocation();
-            console.log("touchend end:"+event.touch.getLocation().x +"|"+event.touch.getLocation().y)
-            var tank = require("Tank");
-            this.refToTank.faceAndfire(location.x, location.y);
-            // this.refToTank.move(event.touch.getLocation().x, event.touch.getLocation().y);
+            // console.log("touchend end:"+event.touch.getLocation().x +"|"+event.touch.getLocation().y)
+            // todo 判断，如果炮塔旋转完毕，已瞄准触屏方向，松开就开炮
+            // console.log("pre move:" + this.pre_move);
+            this.refToTank.fire();
         }
         this.node.on(cc.Node.EventType.TOUCH_START, begin, this);
         this.node.on(cc.Node.EventType.TOUCH_MOVE, move, this);
@@ -33,7 +47,7 @@ cc.Class({
     },
 
     // called every frame, uncomment this function to activate update callback
-    // update: function (dt) {
-
-    // },
+    update: function (dt) {
+        // console.log("touch tick!" + dt);
+    },
 });
