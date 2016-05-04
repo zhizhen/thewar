cf6a2969fd89ed70e5801e90fd8e91cea962e9f6
zhizhen <zhzhzhen@gmail.com>
2016-05-04 16:15:37 +0800

    battle ui and wave show

--- client/Assets/Scripts/Game.js
+++ client/Assets/Scripts/Game.js
@@ -4,7 +4,8 @@ var Game = cc.Class({
     extends: cc.Component,
     
     properties: {
-        
+        inGameUI: cc.Node,
+        waveMgr: cc.Node
     },
     
     // 单例
@@ -16,13 +17,14 @@ var Game = cc.Class({
     onLoad: function () {
         Game.instance = this;
         console.log("游戏启动！");
-        
+        this.waveMgr = this.waveMgr.getComponent('WaveMgr');
+        this.waveMgr.init(this);
     },
     
     start: function() {
-        console.log("send");
-        NetMgr.send("hello pomeloo!!");
-        console.log("sended");
+        this.inGameUI = this.inGameUI.getComponent('InGameUI');
+        this.inGameUI.init(this);
+        this.waveMgr.startWave();
     },
 
     // called every frame, uncomment this function to activate update callback
---
+++ client/Assets/Scripts/WaveMgr.js
@@ -0,0 +1,36 @@
+
+const Wave = cc.Class({
+    name: 'Wave',
+    properties: {
+        
+    },
+    init () {
+        
+    }
+});
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        waves: {
+            default: [],
+            type: Wave
+        },
+        startWaveIdx: 0,
+        spawnMargin: 0,
+        waveProgress: cc.Node
+    },
+
+    // use this for initialization
+    init (game) {
+        this.game = game;
+        // this.waveProgress = this.waveProgress.getComponent('WaveProgress');
+        // this.waveProgress.init(this);
+    },
+
+    startWave () {
+        this.game.inGameUI.showWave(2);
+        
+    }
+});
