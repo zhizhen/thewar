4a6b6ea3438e0d4ed5a8c38370803892725e506d
zhizhen <zhzhzhen@gmail.com>
2016-05-06 19:06:40 +0800

    fix

---
+++ client/Assets/Scripts/Foe.js
@@ -0,0 +1,25 @@
+const FoeType = require('Types').FoeType;
+const AttackType = cc.Enum({
+    Melee: -1,
+    Range: -1
+});
+
+cc.Class({
+    extends: cc.Component,
+    
+    properties: {
+        foeType: {
+            default: FoeType.Foe0,
+            type: FoeType
+        },
+        atkType: {
+            default: AttackType.Melee,
+            type: AttackType
+        }
+    },
+    
+    init (vaveMgr) {
+        cc.log("foe init!");
+        this.waveMgr = waveMgr;
+    }
+});
