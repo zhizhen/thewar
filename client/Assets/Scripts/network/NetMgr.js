e608aca77dd172c6f216f80a3a6ebb0c7a7bded5
zhizhen <zhzhzhen@gmail.com>
2016-05-26 17:27:16 +0800

    protobuf.js test

--- client/Assets/Scripts/network/NetMgr.js
+++ client/Assets/Scripts/network/NetMgr.js
@@ -1,21 +1,30 @@
-// var boot = require('boot')
+var ProtoBuf = require("protobuf");
+var Message = ProtoBuf.loadProtoFile("res/raw-assets/resources/example.proto").build("Message");
+
 var NetMgr = cc.Class({
     ctor: function () {
         cc.log("netmgr constructor!");    // true
-        // Connect to our server: node server.js
-        this.socket = new WebSocket("ws://localhost:8080/ws");
-        this.socket.binaryType = "arraybuffer"; // We are talking binary
-        this.connect();
     },
     
     connect: function(address
     ) {
-        cc.log("on start");
-       
+        cc.log("on connect");
+        // Connect to our server: node server.js
+        this.socket = new WebSocket("ws://localhost:8080/ws");
+        this.socket.binaryType = "arraybuffer"; // We are talking binary
     },
     
-    send: function(Msg) {
-        pomelo.notify("connector.entryHandler.entry", Msg);
+    send: function() {
+        cc.log("on sent message");
+        if (this.socket.readyState == WebSocket.OPEN) {
+            var msg = new Message("hello cocos creator!");
+            cc.log("msg:", msg);
+            var arrayBuffer = msg.toArrayBuffer();
+            this.socket.send(arrayBuffer);
+            cc.log("sent:", msg.text);
+        } else {
+            cc.log("not connected");
+        }
     }
 });
 
