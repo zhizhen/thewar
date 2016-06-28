122722e551dab7b25b2154c4103c258b86f3e685
zhizhen <zhzhzhen@gmail.com>
2016-06-28 11:47:01 +0800

    加载UI

--- client-csharp/Assets/Scripts/GameApp.cs
+++ client-csharp/Assets/Scripts/GameApp.cs
@@ -6,11 +6,33 @@ public class GameApp : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		NetMgr.GetInstance ().connect ("113.105.250.96", 12000);
+        //NetMgr.GetInstance ().connect ("113.105.250.96", 12000);
+        InitSceneCamera();
+        GameUI.InitBaseUI();
+        GameObject.DontDestroyOnLoad(gameObject);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    private void InitSceneCamera()
+    {
+        GameObject mainCameraGo = GameObject.FindWithTag("MainCamera");
+        Camera mainCamera;
+        if (mainCameraGo == null)
+        {
+            mainCameraGo = new GameObject("SceneCamera");
+            GameObject.DontDestroyOnLoad(mainCameraGo);
+            mainCamera = mainCameraGo.AddComponent<Camera>();
+            mainCamera.clearFlags = CameraClearFlags.SolidColor;
+            mainCamera.backgroundColor = Color.black;
+            mainCamera.cullingMask = 0;
+        }
+        else
+        {
+            mainCamera = mainCameraGo.GetComponent<Camera>();
+        }
+    }
 }
---
+++ client-csharp/Assets/Scripts/ui/GameUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameUI {
+    private static GameObject mUIRoot;
+    //private static UIGameLoading mGameLoading = null;
+    private static GameObject mWaitLoading = null;
+    private static Text mWaitLoadingText;
+    //private static UIAlert mAlert;
+
+    public static void InitBaseUI()
+    {
+        if (mUIRoot == null)
+        {
+            mUIRoot = GameObject.Instantiate(Resources.Load("UIRoot")) as GameObject;
+            mUIRoot.name = "UIRoot";
+            GameObject.DontDestroyOnLoad(mUIRoot);
+        }
+    }
+}
