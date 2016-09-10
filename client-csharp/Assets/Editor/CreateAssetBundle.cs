3101e3a3118d3f3295944e5fee6ba743883ece52
zhizhen <zhzhzhen@gmail.com>
2016-09-10 23:36:37 +0800

    capture the camera device

--- client-csharp/Assets/Editor/CreateAssetBundle.cs
+++ client-csharp/Assets/Editor/CreateAssetBundle.cs
@@ -1,29 +1,28 @@
-﻿using UnityEngine;
+﻿/*
+
+using UnityEngine;
 using System.Collections;
 using UnityEditor;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-	[MenuItem("Custom Editor/Create AssetBunldes Main")]
+	[MenuItem ("Custom Editor/Create AssetBunldes Main")]
 	static void CreateAssetBunldesMain ()
 	{
 		//获取在Project视图中选择的所有游戏对象
 		Object[] SelectedAsset = Selection.GetFiltered (typeof(Object), SelectionMode.DeepAssets);
 
 		//遍历所有的游戏对象
-		foreach (Object obj in SelectedAsset) 
-		{
+		foreach (Object obj in SelectedAsset) {
 			string sourcePath = AssetDatabase.GetAssetPath (obj);
 			//本地测试：建议最后将Assetbundle放在StreamingAssets文件夹下，如果没有就创建一个，因为移动平台下只能读取这个路径
 			//StreamingAssets是只读路径，不能写入
 			//服务器下载：就不需要放在这里，服务器上客户端用www类进行下载。
 			string targetPath = Application.dataPath + "/StreamingAssets/" + obj.name + ".assetbundle";
 			if (BuildPipeline.BuildAssetBundle (obj, null, targetPath, BuildAssetBundleOptions.CollectDependencies)) {
-				Debug.Log(obj.name +"资源打包成功");
-			} 
-			else 
-			{
-				Debug.Log(obj.name +"资源打包失败");
+				Debug.Log (obj.name + "资源打包成功");
+			} else {
+				Debug.Log (obj.name + "资源打包失败");
 			}
 		}
 		//刷新编辑器
@@ -31,7 +30,7 @@ public class NewBehaviourScript : MonoBehaviour
 
 	}
 
-	[MenuItem("Custom Editor/Create AssetBunldes ALL")]
+	[MenuItem ("Custom Editor/Create AssetBunldes ALL")]
 	static void CreateAssetBunldesALL ()
 	{
 
@@ -41,8 +40,7 @@ public class NewBehaviourScript : MonoBehaviour
 
 		Object[] SelectedAsset = Selection.GetFiltered (typeof(Object), SelectionMode.DeepAssets);
 
-		foreach (Object obj in SelectedAsset) 
-		{
+		foreach (Object obj in SelectedAsset) {
 			Debug.Log ("Create AssetBunldes name :" + obj);
 		}
 
@@ -54,3 +52,5 @@ public class NewBehaviourScript : MonoBehaviour
 		}
 	}
 }
+
+*/
