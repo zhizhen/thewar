add05531f846b469595fb3a106588e8ab4de25e9
zhizhen <zhzhzhen@gmail.com>
2016-11-02 14:50:16 +0800

    update

--- client-csharp/Assets/Scripts/engine/chara/ShowGSensorValue.cs
+++ client-csharp/Assets/Scripts/engine/chara/ShowGSensorValue.cs
@@ -27,8 +27,13 @@ public class ShowGSensorValue : MonoBehaviour {
 
     void OnGUI()
     {
+#if UNITY_EDITOR
+        GUI.Box(new Rect(5, 5, 100, 20), string.Format("{0:0.000}", Input.GetAxis("Horizontal")));
+        GUI.Box(new Rect(5, 30, 100, 20), string.Format("{0:0.000}", Mathf.Clamp01(Input.GetAxis("Vertical"))));
+#else
         GUI.Box(new Rect(5, 5, 100, 20), string.Format("{0:0.000}", Input.acceleration.x));
         GUI.Box(new Rect(5, 30, 100, 20), string.Format("{0:0.000}", Input.acceleration.y));
         GUI.Box(new Rect(5, 55, 100, 20), string.Format("{0:0.000}", Input.acceleration.z));
+#endif
     }
 }
