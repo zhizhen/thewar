8789237d56a4f0103f37bf854550711a02014746
zhizhen <zhzhzhen@gmail.com>
2016-11-02 15:12:00 +0800

    update

--- client-csharp/Assets/Scripts/engine/chara/ShowGSensorValue.cs
+++ client-csharp/Assets/Scripts/engine/chara/ShowGSensorValue.cs
@@ -3,9 +3,6 @@ using System.Collections;
 
 public class ShowGSensorValue : MonoBehaviour {
 
-    public Rigidbody Target = null;
-    public float ForceFactor = 10.0f;
-
     // Use this for initialization
     void Start () {
 	
@@ -15,15 +12,6 @@ public class ShowGSensorValue : MonoBehaviour {
 	void Update () {
 	
 	}
-
-    // Update is called once per frame  
-    void FixedUpdate()
-    {
-        if (Target != null)
-        {
-            Target.AddForce(new Vector3(Input.acceleration.x, Input.acceleration.z, Input.acceleration.y) * ForceFactor, ForceMode.Force);
-        }
-    }
 
     void OnGUI()
     {
