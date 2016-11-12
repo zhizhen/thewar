0ea3bf4f4c91c8e73c2f28469e0b382fa61b9284
zhizhen <zhzhzhen@gmail.com>
2016-11-12 22:07:26 +0800

    UI

--- client-csharp/Assets/Scripts/modules/battle/BattlePanelView.cs
+++ client-csharp/Assets/Scripts/modules/battle/BattlePanelView.cs
@@ -13,8 +13,12 @@ public class BattlePanelView : Singleton<BattlePanelView>
 
     public BattlePanelView()
     {
-        view = UIManager.Instance.GetSingleUI(UIType.battle);
-        GameObject rootcanvas = GameObject.Find("UIRootCanvas");
+        GameObject UIRootCanvas = GameObject.Find("UIRootCanvas");
+        GameObject UICamera = UIRootCanvas.transform.FindChild("UICamera").gameObject;
+        GameObject view = ResourceMgr.GetGameObject(URLConst.GetUI(UIType.battle.Name));
+        view.transform.SetParent(UIRootCanvas.transform);
+        view.transform.localPosition = new Vector3(0, 0, 0);
+        view.transform.localScale = new Vector3(1, 1, 1);
         m_battleSkillUI = new BattleSkillUI();
     }
 
